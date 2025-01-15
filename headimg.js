376ee01b61dc0b8a85fd51687a7eb8c7164562f1
foxh00 <00henryfox@gmail.com>
2025-01-15 10:54:05 +0000

    Code for header parallax scroll to put on site

---
+++ headimg.js
@@ -0,0 +1,16 @@
+
+function parallax_height() {
+  var scroll_top = $(this).scrollTop();
+  var sample_section_top = $(".sample-section").offset().top;
+  var header_height = $(".visual-header-section").outerHeight();
+  $(".sample-section").css({ "margin-top": header_height });
+  $(".visual-header").css({ height: header_height - scroll_top });
+}
+parallax_height();
+$(window).scroll(function() {
+  parallax_height();
+});
+$(window).resize(function() {
+  parallax_height();
+});
+
