92117d12858d05fb376b78d81b1a039c2c615358
foxh00 <00henryfox@gmail.com>
2025-01-16 23:51:50 +0000

    Delete shoppingcart directory

--- shoppingcart/placeorder.php
+++
@@ -1,8 +0,0 @@
-<?=template_header('Place Order')?>
-
-<div class="placeorder content-wrapper">
-    <h1>Your Order Has Been Placed</h1>
-    <p>Thank you for ordering with MKTime!</p>
-</div>
-
-<?=template_footer()?>
