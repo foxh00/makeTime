5e1a46ae25fe466d3bb5064d5c84b35b495de88e
foxh00 <00henryfox@gmail.com>
2025-01-16 23:54:17 +0000

    Add files via upload

---
+++ shoppingcart/placeorder.php
@@ -0,0 +1,8 @@
+<?=template_header('Place Order')?>
+
+<div class="placeorder content-wrapper">
+    <h1>Your Order Has Been Placed</h1>
+    <p>Thank you for ordering with MKTime!</p>
+</div>
+
+<?=template_footer()?>
